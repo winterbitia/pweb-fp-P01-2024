42c87a72c47c1a8c37c7754ea636e6be078663ad
Faridaqurr <faridaqurr29@gmail.com>
2024-12-19 17:47:17 +0700

    add home page

---
+++ frontend/src/dummyData.js
@@ -0,0 +1,14 @@
+// src/dummyData.js
+export default [
+    {
+      username: "admin",
+      password: "admin123",
+      role: "admin",
+    },
+    {
+      username: "operator",
+      password: "operator123",
+      role: "operator",
+    },
+  ];
+  
