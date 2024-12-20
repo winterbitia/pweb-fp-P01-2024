3a5c39e3cb7d69cd04b94298abc1727efa81b9ea
Faridaqurr <faridaqurr29@gmail.com>
2024-12-21 02:11:37 +0700

    add login+route

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,8 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import LandingView from '@/views/LandingView.vue'
-import AdminItems from '@/views/AdminItems.vue'
-import AdminView from '@/views/AdminView.vue'
-import OperatorView from '@/views/OperatorView.vue'
+import { defineAsyncComponent } from 'vue'
+
+const LandingView = defineAsyncComponent(() => import('../views/LandingView.vue'))
+const AdminItems = defineAsyncComponent(() => import('../views/AdminItems.vue'))
+const AdminView = defineAsyncComponent(() => import('../views/AdminView.vue'))
+const OperatorView = defineAsyncComponent(() => import('../views/OperatorView.vue'))
+const RulesView = defineAsyncComponent(() => import('../views/RulesView.vue')) // Import RulesView
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -31,6 +34,11 @@ const router = createRouter({
       path: '/operator',
       name: 'Operator',
       component: OperatorView,
+    },
+    {
+      path: '/rules', // Add new route for rules
+      name: 'Rules',
+      component: RulesView,
     },
   ],
 })
